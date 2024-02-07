48af5f910f7f11534d2dba6a7cd712c59e7a6237
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-02-07 11:15:26 +0700

    update:Add->Hooks

---
+++ hooks/useInterval.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from "react";
+
+export default function useInterval(callback: () => void, delay: number) {
+  const savedCallback = useRef<() => void>(callback);
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    const tick = () => {
+      savedCallback.current();
+    };
+
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
