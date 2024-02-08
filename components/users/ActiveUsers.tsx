9f20583607b9a1fac49a0034f22751084fd62d79
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-02-08 12:27:32 +0700

    update:Add->UserAvt

---
+++ components/users/ActiveUsers.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React, { useMemo } from 'react';
+import Avatar from './Avatar';
+import { generateRandomName } from '@/lib/utils';
+import { useOthers, useSelf } from '@/liveblocks.config';
+
+const ActiveUsers = () => {
+  return (
+    <div>ActiveUsers</div>
+  );
+};
+
+export default ActiveUsers;
---
+++ components/users/Avatar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Image from "next/image";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+
+type Props = {
+  name: string;
+  otherStyles?: string;
+};
+
+const Avatar = ({ name, otherStyles }: Props) => {
+  return (
+    <>
+      <Tooltip>
+        <TooltipTrigger>
+          <div 
+            className={`relative h-9 w-9 rounded-full ${otherStyles}`} 
+            data-tooltip={name}
+          >
+
+          </div>
+        </TooltipTrigger>
+        <TooltipContent className='border-none bg-primary-grey-200 px-2 py-1.5 text-xs
+        '>
+          {name}
+        </TooltipContent>
+      </Tooltip>
+    </>
+  );
+};
+
+export default Avatar;
