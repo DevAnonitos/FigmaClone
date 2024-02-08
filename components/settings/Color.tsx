4f20756595e0af29b37a5f64f314e6f3efb703e8
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-02-08 16:23:04 +0700

    update:Add->SettingComponents

---
+++ components/settings/Color.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Color = () => {
+  return (
+    <div>Color</div>
+  );
+};
+
+export default Color;
---
+++ components/settings/Dimensions.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dimensions = () => {
+  return (
+    <div>Dimensions</div>
+  );
+};
+
+export default Dimensions;
