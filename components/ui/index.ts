e3cf04eae8782b8556876c8103d683d584651880
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-02-12 00:32:32 +0700

    update:Add->ConfigExportComponents

---
+++ components/ui/index.ts
@@ -0,0 +1,19 @@
+import { Button } from "./button";
+import { Collapsible } from "./collapsible";
+import { ContextMenu } from "./context-menu";
+import { DropdownMenu } from "./dropdown-menu";
+import { Input } from "./input";
+import { Label } from "./label";
+import { Select } from "./select";
+import { Tooltip } from "./tooltip";
+
+export {
+  Button,
+  Collapsible,
+  ContextMenu,
+  DropdownMenu,
+  Input,
+  Label,
+  Select,
+  Tooltip,
+};
