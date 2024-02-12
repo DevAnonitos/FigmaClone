7424081c72116d0e3c3dd4bbdf881d60a401d949
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-02-12 22:23:19 +0700

    update:Add->CanvasLib

---
+++ lib/canvas.ts
@@ -0,0 +1,24 @@
+import { fabric } from "fabric";
+import { v4 as uuidv4 } from "uuid"; 
+
+import { 
+  CanvasMouseDown, 
+  CanvasMouseMove, 
+  CanvasMouseUp, 
+  CanvasObjectModified, 
+  CanvasObjectScaling, 
+  CanvasPathCreated, 
+  CanvasSelectionCreated 
+} from "@/types/type";
+
+import { defaultNavElement } from "@/constants";
+import { createSpecificShape } from "./shapes";
+
+export const initializeFabric = () => {
+
+};
+
+export const handleCanvasMouseDown = () => {
+
+};
+
