00b4598745a1b75d01dac71a81a7aefc5cb25bff
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-02-13 00:36:55 +0700

    update:Add->PinnedComposer

--- components/comments/PinnedComposer.tsx
+++ components/comments/PinnedComposer.tsx
@@ -1,8 +1,36 @@
-import React from 'react';
+"use client";
 
-const PinnedComposer = () => {
+import React from 'react';
+import { Composer, ComposerProps } from '@liveblocks/react-comments';
+
+type Props = {
+  onComposerSubmit: ComposerProps["onComposerSubmit"];
+};
+
+const PinnedComposer = ({ onComposerSubmit, ...props }: Props) => {
   return (
-    <div>PinnedComposer</div>
+    <>
+      <div className='absolute flex gap-4' {...props}>
+        <div 
+          className='select-none relative w-9 h-9 shadow rounded-tr-full rounded-br-full rounded-bl-full bg-white flex justify-center 
+          items-center'
+        >
+
+        </div>
+        <div 
+          className='shadow bg-white rounded-lg flex flex-col 
+          text-sm min-w-96 overflow-hidden p-2'
+        >
+          <Composer
+            onComposerSubmit={onComposerSubmit}
+            autoFocus={true}
+            onKeyUp={(e) => {
+              e.stopPropagation()
+            }}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
