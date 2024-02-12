1adcc358beabaa5f5287b1b9c140b0df4e051c27
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-02-12 22:18:33 +0700

    update:Add->ConfigLibs

---
+++ lib/key-events.ts
@@ -0,0 +1,20 @@
+import { fabric } from "fabric";
+import { v4 as uuidv4 } from "uuid";
+
+export const handleCopy = () => {
+
+};
+
+export const handlePaste = () => {
+
+};
+
+
+export const handleDelete = () => {
+
+};
+
+export const handleKeyDown = () => {
+
+};
+
---
+++ lib/shapes.ts
@@ -0,0 +1,50 @@
+import { fabric } from "fabric"; 
+import { v4 as uuidv4 } from "uuid";
+
+import { 
+  CustomFabricObject, 
+  ElementDirection, 
+  ImageUpload, 
+  ModifyShape 
+} from "@/types/type";
+
+export const createRectangle = () => {
+
+};
+
+export const createTriangle = () => {
+
+};
+
+export const createCircle = () => {
+
+};
+
+export const createLine = () => {
+
+};
+
+export const createText = () => {
+
+};
+
+export const createSpecificShape = () => {
+
+};
+
+export const handleImageUpload = () => {
+
+};
+
+export const createShape = () => {
+
+};
+
+export const modifyShape = () => {
+
+};
+
+export const bringElement = () => {
+
+};
+
