8429aa4c2b8534dff813c55750b10496823f92e1
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-02-12 23:43:16 +0700

    update:Add->NewThread

--- components/comments/NewThread.tsx
+++ components/comments/NewThread.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+"use client";
 
-const NewThread = () => {
+import React, { 
+  FormEvent, 
+  ReactNode, 
+  useCallback, 
+  useEffect, 
+  useRef, 
+  useState 
+} from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import * as Portal from "@radix-ui/react-portal";
+import { ComposerSubmitComment } from "@liveblocks/react-comments/primitives";
+
+import { useCreateThread } from '@/liveblocks.config';
+import { useMaxIndex } from '@/lib/useMaxZIndex';
+
+import PinnedComposer from './PinnedComposer';
+import NewThreadCursor from './NewThreadCursor';
+
+type Props = {
+  children: ReactNode;
+};  
+
+const NewThread = ({ children }: Props) => {
   return (
-    <div>NewThread</div>
+    <>
+      <Slot>
+        {children}
+      </Slot>
+    </>
   );
 };
 
