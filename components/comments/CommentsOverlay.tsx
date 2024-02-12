463f64845ce6f0f19147de69b24847fa1a71fdb6
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-02-12 23:20:32 +0700

    update:Add->CommentOverlay

--- components/comments/CommentsOverlay.tsx
+++ components/comments/CommentsOverlay.tsx
@@ -1,6 +1,37 @@
-import React from 'react';
+"use client";
+
+import React, { useCallback, useRef } from 'react';
+import { ThreadData } from '@liveblocks/client';
+import { useMaxIndex } from '@/lib/useMaxZIndex';
+
+import PinnedThread from './PinnedThread';
+import { 
+  ThreadMetadata, 
+  useEditThreadMetadata, 
+  useThreads, 
+  useUser, 
+} from '../../liveblocks.config';
+
+type OverplayThreadProps = {
+  thread: ThreadData<ThreadMetadata>
+  maxZIndex: number;
+};
+
+const overlayThread = ({ thread, maxZIndex }: OverplayThreadProps) => {
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  );
+};
 
 const CommentsOverlay = () => {
+
+  const { threads } = useThreads();
+  const maxIndex = useMaxIndex();
+  
   return (
     <div>CommentsOverlay</div>
   );
