4126b066b0a06d243411c2f0fe50d756162937a9
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-02-14 21:32:17 +0700

    update:Add->ConfigType-keyEvent

--- lib/key-events.ts
+++ lib/key-events.ts
@@ -1,20 +1,43 @@
 import { fabric } from "fabric";
 import { v4 as uuidv4 } from "uuid";
 
-export const handleCopy = () => {
+import { CustomFabricObject } from "@/types/type";
+
+export const handleCopy = (canvas: fabric.Canvas) => {
 
 };
 
-export const handlePaste = () => {
+export const handlePaste = (
+  canvas: fabric.Canvas, 
+  syncShapeInStorage: (shape: fabric.Object) => void
+) => {
+
 
 };
 
 
-export const handleDelete = () => {
+export const handleDelete = (
+  canvas: fabric.Canvas, 
+  deleteShapeFromStorage: (id: string) => void
+) => {
 
 };
 
-export const handleKeyDown = () => {
+export const handleKeyDown = ({ 
+  e,
+  canvas,
+  undo,
+  redo,
+  syncShapeInStorage,
+  deleteShapeFromStorage,
+}: {
+  e: KeyboardEvent,
+  canvas: fabric.Canvas | any,
+  undo: () => void,
+  redo: () => void,
+  syncShapeInStorage: (shape: fabric.Object) => void,
+  deleteShapeFromStorage: (id: string) => void
+}) => {
 
 };
 
