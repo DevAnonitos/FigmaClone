c322055cce89b99d8a3fb6fc99813f223c30b662
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-02-17 16:05:21 +0700

    update:Add->FuncCanvas

--- lib/canvas.ts
+++ lib/canvas.ts
@@ -8,17 +8,101 @@ import {
   CanvasObjectModified, 
   CanvasObjectScaling, 
   CanvasPathCreated, 
-  CanvasSelectionCreated 
+  CanvasSelectionCreated, 
+  RenderCanvas,
 } from "@/types/type";
 
 import { defaultNavElement } from "@/constants";
 import { createSpecificShape } from "./shapes";
+import React from "react";
 
-export const initializeFabric = () => {
+export const initializeFabric = ({
+  fabricRef,
+  canvasRef,
+}: {
+  fabricRef: React.MutableRefObject<fabric.Canvas | null>,
+  canvasRef: React.MutableRefObject<HTMLCanvasElement | null>,
+}) => {
 
 };
 
-export const handleCanvasMouseDown = () => {
+export const handleCanvasMouseDown = ({ 
+  options,
+  canvas,
+  selectedShapeRef,
+  isDrawing,
+  shapeRef,
+}: CanvasMouseDown) => {
+
+};
+
+export const handleCanvasMouseMove = ({ 
+  options,
+  canvas,
+  isDrawing,
+  selectedShapeRef,
+  shapeRef,
+  syncShapeInStorage,
+}: CanvasMouseMove) => {
+
+};
+
+export const handleCanvasMouseUp = ({ 
+  canvas,
+  isDrawing,
+  shapeRef,
+  activeObjectRef,
+  selectedShapeRef,
+  syncShapeInStorage,
+  setActiveElement,
+}: CanvasMouseUp) => {
+
+};
+
+export const handleCanvasObjectMoving = ({ 
+  options,
+}: { options: fabric.IEvent }) => {
+
+};
+
+export const handleCanvasSelectionCreated = ({ 
+  options,
+  isEditingRef,
+  setElementAttributes,
+}: CanvasSelectionCreated) => {
+
+};
+
+export const handleCanvasObjectScaling = ({ 
+  options,
+  setElementAttributes,
+}: CanvasObjectScaling) => {
+
+};
+
+export const renderCanvas = ({ 
+  fabricRef,
+  canvasObjects,
+  activeObjectRef,
+}: RenderCanvas) => {
+  
+};
+
+export const handleResize = ({ 
+  canvas 
+}: { 
+  canvas: fabric.Canvas | null 
+}) => {
+
+};
+
+export const handleCanvasZoom = ({ 
+  options,
+  canvas,
+}: { 
+  options: fabric.IEvent & { e: WheelEvent },
+  canvas: fabric.Canvas
+}) => {
 
 };
 
