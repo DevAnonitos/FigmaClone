4c0ada91ab3f7783e75c266c2f6112cd9b0fd7e9
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-02-17 11:25:33 +0700

    update:Add->SimpleShapes

--- lib/shapes.ts
+++ lib/shapes.ts
@@ -9,23 +9,68 @@ import {
 } from "@/types/type";
 
 export const createRectangle = (pointer: PointerEvent) => {
+  const rect = new fabric.Rect({
+    left: pointer.x,
+    top: pointer.y,
+    width: 100,
+    height: 100,
+    fill: "#aabbcc",
+    objectId: uuidv4(),
+  } as CustomFabricObject<fabric.Rect>);
 
+  return rect;
 };
 
 export const createTriangle = (pointer: PointerEvent) => {
-
+  const triangle = new fabric.Triangle({
+    left: pointer.x,
+    top: pointer.y,
+    width: 100,
+    height: 100,
+    fill: "#aabbcc",
+    objectId: uuidv4(),
+  } as CustomFabricObject<fabric.Triangle>); 
+  
+  return triangle;
 };
 
 export const createCircle = (pointer: PointerEvent) => {
+  const circle =  new fabric.Circle({
+    left: pointer.x,
+    top: pointer.y,
+    radius: 100,
+    fill: "#aabbcc",
+    objectId: uuidv4(),
+  } as any);
 
+  return circle;
 };
 
 export const createLine = (pointer: PointerEvent) => {
+  const line = new fabric.Line(
+    [pointer.x, pointer.y, pointer.x + 100, pointer.y + 100],
+    {
+      stroke: "#aabbcc",
+      strokeWidth: 2,
+      objectId: uuidv4(),
+    } as CustomFabricObject<fabric.Line> 
+  );
 
+  return line;
 };
 
 export const createText = (pointer: PointerEvent, text: string) => {
+  const txt = new fabric.Text(text, {
+    left: pointer.x,
+    top: pointer.y,
+    fill: "#aabbcc",
+    fontFamily: 'Helvetica',
+    fontSize: 36,
+    fontWeight: '400',
+    objectId: uuidv4(),
+  } as fabric.ITextOptions);
 
+  return txt;
 };
 
 export const createSpecificShape = (shapeType: string, pointer: PointerEvent) => {
