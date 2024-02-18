41381529a8546ab1f4d46971a9199617f678fc83
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-02-18 10:58:18 +0700

    update:Add->RightSideBar-useMemo

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -20,34 +20,35 @@ const RightSidebar = ({
   syncShapeInStorage,
 }: RightSidebarProps) => {
 
-  
-  return (
-    <>
-      <section 
-        className='flex flex-col border-r border-l-[1px] border-gray-700  bg-primary-black text-primary-grey-300 min-w-[227px] sticky right-0 
-        h-full max-sm:hidden select-none'
+  const memoizedContent = useMemo(() => (
+    
+    <section 
+      className='flex flex-col border-r border-l-[1px] border-gray-700  
+      bg-primary-black text-primary-grey-300 min-w-[227px] sticky right-0 
+      h-full max-sm:hidden select-none'
+    >
+      <h3 className='px-5 pt-4 text-xs uppercase'>
+        Design
+      </h3> 
+      <span 
+        className='text-xs text-primary-grey-300 mt-3 
+        px-5 border-b-[1px] border-gray-700 pb-4'
       >
-        <h3 className='px-5 pt-4 text-xs uppercase'>
-          Design
-        </h3> 
-        <span 
-          className='text-xs text-primary-grey-300 mt-3 
-          px-5 border-b-[1px] border-gray-700 pb-4'
-        >
         Make changes to canvas as you like
-        </span>
+      </span>
 
-        <Dimensions />
+      <Dimensions />
 
-        <Text />
+      <Text />
         
-        <Color />
-        <Color />
+      <Color />
+      <Color />
 
-        <Export />
-      </section>
-    </>
-  );
+      <Export />
+    </section>
+  ), [elementAttributes]);
+
+  return memoizedContent;
 };
 
 export default RightSidebar;
